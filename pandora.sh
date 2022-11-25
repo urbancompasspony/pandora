33d95ea3fee70101e2e697ec270160148b3ae6d2
Nathan Drake <32694176+urbancompasspony@users.noreply.github.com>
2022-11-24 22:41:20 -0300

    Update pandora.sh

--- pandora.sh
+++ pandora.sh
@@ -1,58 +1,38 @@
 #!/bin/bash
 
-# Project Pandora ##############################################################
-############################################################## By Nathan Drake #
-
-################################################################################
-#                                                                              #
-# Dependencies:                                                                #
-# parallel nmap metasploit mutt ntp cyrus-common iproute2 zip sendmail         #
-# apt purge postfix                                                            #
-# Do not forget about /root/.mutt!                                             #
-#                                                                              #
+###################
+# Project Pandora #
 ################################################################################
 
 # Name for this Pandora device.
 namepan=$(cat /Data/hostname)
 
-# How much parallel jobs will run at time.
-RUNA=$(cat /Data/runa)
-
 # ntfy server:
 ntfysh=$(cat /Data/ntfysh)
 
+# How much parallel jobs will run at time.
+RUNA=$(cat /Data/runa)
+
+# PID FILE
+pidfile="/Pentests"
+
 ################################################################################
-
-# Custom path for PENTESTS results
-pathtest="/Pentests/PENTESTS"
-
-# Custom path for ZIPPED files from results.
-zipfiles="/Pentests/ZIP"
-
-# SUDO check!?
-[ "$EUID" -ne 0 ] && {
-  echo "Run this script as Root! Exiting..."
-  exit
-  } || {
-    echo "Nothing" > /dev/null
-    }
 
 function init {
   # Kill nmap after X seconds if hang!
-  sleep 1800 && pkill nmap & echo $! | tee /Pentests/.pndr.pid
+  sleep 1800 && pkill nmap & echo $! | tee "$pidfile"/.pndr.pid
 
   # Set some vars
-  datetime=$(date +"%d/%m %H:%M")
-  name=$(date +"%d%m%y-%H%M%S")
-  vuln0="$pathtest/$name/VULN"
+  datetime=$(date +"%d/%m/%y %H:%M")
+  name=$(date +"%d_%m_%y-%H:%M")
 
   # Create main dir, if it does not exist
-  mkdir -p "$zipfiles/$name/VULN"
-  mkdir -p "$pathtest/$name"
+  mkdir -p "$zipfiles"
+  mkdir -p "$pathtest"/"$name"
 
   # Generate some Files and Vars
-  touch "$pathtest/$name/01IP"; toip="$pathtest/$name/01IP"
-  touch "$pathtest/$name/02Log"; tolog="$pathtest/$name/02Log"
+  touch "$pathtest"/"$name"/01IP; toip="$pathtest"/"$name"/01IP
+  touch "$pathtest"/"$name"/02Log; tolog="$pathtest"/"$name"/02Log
 
   # Some logs
   echo "Test started at $datetime!" | tee -a "$tolog"
@@ -68,22 +48,22 @@ function init {
   cat "$toip" | parallel -j "$RUNA" -k "nmap -Pn --script vuln {} | tee -a $pathtest/$name/{}"
 
   # When finished
-  datetime2=$(date +"%d/%m %H:%M")
+  datetime2=$(date +"%d/%m/%y %H:%M")
 
   # Just some last logs to finish this.
   echo "This test ran from $datetime to $datetime2." | tee -a "$tolog"
 
   # Kill NMAP killer!
-  pidsleep=$(cat /Pentests/.pndr.pid)
+  pidsleep=$(cat $pidfile/.pndr.pid)
   echo "Killing PID $pidsleep of sleep_&_auto_kill nmap process" | tee -a "$tolog" 
   kill -9 "$pidsleep"
   pkill sleep
-  rm "/Pentests/.pndr.pid"
+  rm "$pidfile"/.pndr.pid
 
-  # Identify if there is any VULNERABLE!
+  # Identify if there is any VULNERABLE system!
   while read line
   do
-    grep -Fq "VULNERABLE" "$pathtest/$name/$line" && mkdir -p "$vuln0" && mv "$pathtest/$name/$line" "$vuln0" || echo "Nothing found!" > /dev/null
+    grep -Fq "VULNERABLE" "$pathtest/$name/$line" && mkdir -p "$vuln0" && cp "$pathtest/$name/$line" "$vuln0" || echo "Nothing found!" > /dev/null
   done < "$toip"
   sleep 1
 
@@ -97,27 +77,55 @@ function init {
   sleep 1
   zip -r "$zipfiles/$name.zip" "$pathtest/$name"
 
-  [ -d "$vuln0" ] && {
-    mkdir -p "$zipfiles/$name"
-    zip -r "$zipfiles/$name/VULN.zip" "$vuln0"
-  } || {
-    echo "No VULN found!" | tee -a "$tolog"
-  }
-
   sleep 1
 
   # Change permissions
   chmod 777 -R "$pathtest"
 
+  # Remove old Files
+  find "$zipfiles" -type d -mtime +30 -exec rm -rf {} \;
+  find "$zipfiles" -type f -mtime +30 -delete
+  find "$zipfiles" -type d -empty -delete
+
+  find "$pathtest" -type d -mtime +30 -exec rm -rf {} \;
+  find "$pathtest" -type f -mtime +30 -delete
+  find "$pathtest" -type d -empty -delete
+
   # Send message with attachments
   sleep 1
 
-  curl -u admin:5V06auso -T $zipfiles/$name.zip -H "Filename: $name.zip" "$ntfysh"/"$namepan"
+  tontfy=$(cat /Data/ntfysh)
+  [ "$tontfy" == "0" ] && {
+    echo "The End!" > /dev/null
+  } || {
+    [ -d "$vuln0" ] && {
+      curl -u admin:5V06auso -T "$zipfiles"/$name.zip -H "Filename: $name.zip" "$ntfysh"/"$namepan"
+    } || {
+      echo "There's none VULNERABLE detected!" > /dev/null
+    }
+  }
 
 exit 1
 }
 
+# SUDO check!?
+[ "$EUID" -ne 0 ] && {
+  echo "Run this script as Root! Exiting..."
+  exit
+  } || {
+    echo "Nothing" > /dev/null
+    }
+
+# Vulnerable Systems!
+vuln0="$pidfile/VULNERABLE_SYSTEMS"
+
+# Custom path for PENTESTS results
+pathtest="$pidfile/PENTESTS"
+
+# Custom path for ZIPPED files from results.
+zipfiles="$pidfile/ZIP"
+
 # Start all here
 init
 
-exit 0
+exit 1
